915158c8c37161e1f4a43736c4c7526f281f95bf
rk <48819681+rksan@users.noreply.github.com>
2023-07-20 11:07:43 +0900

    setup coverall

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,8 +1,10 @@
-var assert = require("assert");
-describe("Array", function () {
-  describe("#indexOf()", function () {
-    it("should return -1 when the value is not present", function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+const { describe, it } = require("mocha");
+const { assert } = require("chai");
+
+describe("test index", () => {
+  const index = () => {};
+
+  it("index is function", () => {
+    assert.isFunction(index);
   });
 });
