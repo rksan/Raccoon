32a25a0663dd9b36e7530d10e4e9aaf9c81550d7
rk <48819681+rksan@users.noreply.github.com>
2023-07-20 13:18:01 +0900

    update nyc settings

--- test/test.spec.js
+++ test/test.spec.js
@@ -2,9 +2,13 @@ const { describe, it } = require("mocha");
 const { assert } = require("chai");
 
 describe("test index", () => {
-  const index = () => {};
+  const index = require("./test");
 
   it("index is function", () => {
     assert.isFunction(index);
+  });
+
+  it("index call", () => {
+    assert.isTrue(index(), "fail");
   });
 });
