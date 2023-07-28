1370e0308f13436a812e80daa4084463e38ad554
rk <48819681+rksan@users.noreply.github.com>
2023-07-28 09:34:33 +0900

    change github actions

--- jest.config.ts
+++ jest.config.ts
@@ -67,12 +67,7 @@ const config = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.json",
-      useESM: true,
-    },
-  },
+  //globals: {},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -184,7 +179,7 @@ const config = {
   //transform: undefined,
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest",
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.json", useESM: true }],
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
